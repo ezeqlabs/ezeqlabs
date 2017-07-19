8b322ba1281e123fd5091218984df9f10612da1d
Kako Botasso <kakodev@gmail.com>
2017-07-19 20:28:54 -0300

    Salva os contatos no firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,34 @@
+var config = {
+  apiKey: "AIzaSyCwHH6PTa-ixD_c2smaiRT5oWvTjnwawk4",
+  authDomain: "ezeqlabs-d0129.firebaseapp.com",
+  databaseURL: "https://ezeqlabs-d0129.firebaseio.com",
+  projectId: "ezeqlabs-d0129",
+  storageBucket: "",
+  messagingSenderId: "788712698661"
+};
+
+firebase.initializeApp(config);
+
+/* ********************************************************
+***** CADASTRO DE CONTATOS
+******************************************************** */
+
+function criaContato(nome, email, mensagem){
+  var db = firebase.database();
+  var ref = db.ref("contatos");
+  var data = new Date();
+  var dia = data.getDate();
+  var mes = data.getMonth();
+  var ano = data.getFullYear();
+  var dataAtual = ano+"-"+(mes+1)+"-"+dia;
+  var timeStamp = data.getTime()
+
+  ref.push({
+    [timeStamp]: {
+      "nome": nome,
+      "email": email,
+      "mensagem": mensagem,
+      "data": dataAtual
+    }
+  });
+}
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,4 @@
 $(function(){
-  emailjs.init("user_AOUh8L3hN5TQVp9ZO4eWs");
-
   function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
@@ -19,22 +17,22 @@ $(function(){
 
   $('#btForm').click(function(){
     if( $('#campoNome').val() != '' ){
+      var nome = $('#campoNome').val();
 
       if( $('#campoEmail').val() != '' ){
 
         if( isEmail($('#campoEmail').val()) ){
+          var email = $('#campoEmail').val();
 
           if( $('#campoMensagem').val() != '' ){
+            var mensagem = $('#campoMensagem').val();
 
-            emailjs.send("gmail", "template_padrao", { "nome": $('#campoNome').val(), "email": $('#campoEmail').val(), "mensagem": $('#campoMensagem').val() }).then(function(response) {
-             alert('Email enviado! Em breve retornaremos seu contato.');
-             $('#campoNome').val('');
-             $('#campoEmail').val('');
-             $('#campoMensagem').val('');
-             $('.modal').fadeOut(250);
-            }, function(err) {
-             alert('Erro ao enviar email. Tente novamente mais tarde.');
-            });
+            criaContato(nome, email, mensagem)
+            alert('Contato enviado com sucesso! Em breve retornaremos seu contato.');
+            $('#campoNome').val('');
+            $('#campoEmail').val('');
+            $('#campoMensagem').val('');
+            $('.modal').fadeOut(250);
 
           }else{
             alert('Por favor, deixe uma mensagem');
