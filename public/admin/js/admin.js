fab51fb1ba98a73a62bcfcbe54c01b6c5e70834b
Kako Botasso <kakodev@gmail.com>
2017-07-20 02:57:11 -0300

    Inicia a migração para Angular

--- public/admin/js/admin.js
+++ public/admin/js/admin.js
@@ -1,1 +1,19 @@
-$(function(){});
+var app = angular.module('appAdminEzeqlabs', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'dashboard.html'
+    })
+    .when('/dashboard', {
+      templateUrl: 'dashboard.html'
+    })
+    .when('/contatos', {
+      templateUrl: 'contatos.html'
+    })
+    .otherwise({redirectTo: '/'})
+}])
+.controller('Contatos', [function () {
+  var self = this;
+  self.listaContatos = contatos.todosContatos();
+}])
--- public/admin/js/contatos.js
+++ public/admin/js/contatos.js
@@ -1,25 +1,17 @@
-$(function(){
+var contatos = (function(){
+  var c = {};
 
-  carregaContatos(function(contatos){
-    var _html = '';
-
-    if(contatos.length > 0){
+  c.todosContatos = function(){
+    carregaContatos(function(contatos){
+      var listaContatos = [];
       for(var contato in contatos){
         contato = contatos[contato]
-        _html += '<div class="list-group-item list-group-item-action flex-column align-items-start">';
-        _html += '  <div class="d-flex w-100 justify-content-between">';
-        _html += '    <h5 class="mb-1">'+ contato.nome +'</h5>';
-        _html += '    <small>'+ contato.data +'</small>';
-        _html += '  </div>';
-        _html += '  <p class="mb-1">'+ contato.mensagem +'</p>';
-        _html += '  <small>'+ contato.email +'</small>';
-        _html += '</div>';
+        listaContatos.push(contato);
       }
-    } else {
-      _html += '<div class="list-group-item">Nenhum contato cadastrado</div>'
-    }
+      return listaContatos;
+    });
 
-    $('#listaContatos').html(_html);
-  });
+  }
 
-});
+  return c;
+}());
