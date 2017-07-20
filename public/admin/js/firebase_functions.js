152c5c6c3c97741f0d8838dd97ea3b0209da07c9
Kako Botasso <kakodev@gmail.com>
2017-07-20 01:17:56 -0300

    Cria listagem de contatos no admin

--- public/admin/js/firebase_functions.js
+++ public/admin/js/firebase_functions.js
@@ -7,3 +7,21 @@ function fazLogin(email, senha, callback){
       callback(error)
     });
 }
+
+function carregaContatos(callback){
+  firebase.database().ref('contatos/').once('value').then(function(snapshot) {
+    var contatos = [];
+    var objeto = snapshot.val()
+
+    for(var key in objeto){
+      var contato = {
+        nome: objeto[key]["nome"],
+        email: objeto[key]["email"],
+        mensagem: objeto[key]["mensagem"],
+        data: objeto[key]["data"]
+      }
+      contatos.push(contato)
+    }
+    callback(contatos);
+  });
+}
