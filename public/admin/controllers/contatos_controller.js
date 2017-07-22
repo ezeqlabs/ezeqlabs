4d8871aa798bacd491c6b91a8c79154cd67cb709
Kako Botasso <kakodev@gmail.com>
2017-07-22 03:35:26 -0300

    Corrige exibição de contatos no admin

--- public/admin/controllers/contatos_controller.js
+++ public/admin/controllers/contatos_controller.js
@@ -6,7 +6,21 @@ app.controller('Contatos',
       }
 
       var contatos = firebase.database().ref().child('contatos');
-      $scope.contatos = $firebaseArray(contatos);
+      var contatosFirebase = $firebaseArray(contatos);
+      $scope.contatos = contatosFirebase;
+
+      var quantidadeContatos = 0;
+      contatosFirebase.$loaded()
+        .then(function(){
+            angular.forEach(contatosFirebase, function(contato) {
+                quantidadeContatos += 1;
+            })
+            $scope.temContatos = function(){
+              return quantidadeContatos > 0;
+            };
+        });
+
+
     }
   ]
 );
