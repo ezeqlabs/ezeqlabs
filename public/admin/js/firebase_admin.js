e504c9502bb46ff3f1e382bfd25f32e0d910b815
Kako Botasso <kakodev@gmail.com>
2017-07-22 02:14:33 -0300

    Remove arquivos que não estão sendo utilizados

--- public/admin/js/firebase_admin.js
+++
@@ -1,9 +0,0 @@
-function fazLogin(email, senha, callback){
-  firebase.auth().signInWithEmailAndPassword(email, senha)
-    .then(function(){
-      callback(firebase.auth().currentUser);
-    })
-    .catch(function(error) {
-      callback(error)
-    });
-}
